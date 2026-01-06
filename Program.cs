dc3339fefdcae3b22c76e49afd64403e5b2f61f1
Tobiah-Wittenbrink <28wittenbrinkt@newbremenschools.org>
2026-01-06 19:13:59 +0000

    zesty strawberry

--- Program.cs
+++ Program.cs
@@ -1,34 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.Urls.Add("http://localhost:5000");
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/{cityName}/weather", GetWeatherByCity);
-
-app.Run();
-
-
-Weather GetWeatherByCity(string cityName)
+Console.Write("What is your favorite flavor of ice cream?");
+string? favorite = Console.ReadLine();
+if (favorite == null)
 {
-    app.Logger.LogInformation($"Weather requested for {cityName}.");
-    var weather = new Weather(cityName);
-    return weather;
+    favorite = "vanilla";
 }
-
-public record Weather
+if (favorite == "vanilla")
 {
-    public string City { get; set; }
-
-    public Weather(string city)
-    {
-        City = city;
-        Conditions = "Cloudy";
-        // Temperature here is in celsius degrees, hence the 0-40 range.
-        Temperature = new Random().Next(0,40).ToString();
-    }
-
-    public string Conditions { get; set; }
-    public string Temperature { get; set; }
+    Console.WriteLine("Your basic");
 }
+if (favorite == "strawberry")
+{
+    Console.WriteLine("Your fruity :(");
+}
+Console.WriteLine($"You said your favorite flavor of ice cream is {favorite}.");
